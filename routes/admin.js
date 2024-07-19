e67963180ece81acb11f8cabfa28bc99ed0b2ba4
Aqeel Malik <malik.aqeelarshad@gmail.com>
2024-07-19 12:22:46 +0400

    admin urls and routes simplify

--- routes/admin.js
+++ routes/admin.js
@@ -3,17 +3,19 @@ const adminRoutes = Router();
 
 const { users, fnUser } = require("../helpers");
 
-adminRoutes.get("/", (req, res) => {
-   console.log(req.originalUrl); // /admin
-   res.send("Admin Homepage");
-});
-adminRoutes.get("/users", (req, res) => {
-   console.log(req.originalUrl); // /admin/users
-   res.json(users);
-});
-// admin users dynamic urls: /admin/users/:id
-adminRoutes.route("/users/:id").get((req, res) => {
-   res.json(fnUser(req.params.id));
-});
+adminRoutes
+   .get("/", (req, res) => {
+      console.log(req.originalUrl); // /admin
+      res.send("Admin Homepage");
+   })
+   .get("/users", (req, res) => {
+      console.log(req.originalUrl); // /admin/users
+      res.json(users);
+   })
+   .route("/users/:id")
+   .get((req, res) => {
+      // admin users dynamic urls: /admin/users/:id
+      res.json(fnUser(req.params.id));
+   });
 
 module.exports = adminRoutes;
